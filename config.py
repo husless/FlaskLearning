29e4b407290ee34ea8f7839b956ff2bf082d3c13
Huang Zhi <hszhi@Debian-server.debian-server.localhost.domain>
2014-09-09 01:40:22 +0800

    add 404 page and 500 page

--- config.py
+++ config.py
@@ -10,6 +10,15 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 OPENID_PROVIDERS = [
     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
-    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'AOL', 'url': 'https://api.screenname.aol.com/auth/openid/name/<username>' },
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'Weibo', 'url': 'https://api.weibo.com/oauth2/authorize'},]
+
+# mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# administrator list
+ADMINS = ['systmdenable@aol.com']
